982c48f8a2da63e45639ab9f986114fd80bf6e58
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2019-05-29 15:00:08 +0800

    finish data presolve

--- xumw/pcapParse.py
+++ xumw/pcapParse.py
@@ -16,40 +16,38 @@ from sklearn.model_selection  import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 
 
-# f = open('./pcaptest_20190516/0.pcap', 'rb')
-# pcap = dpkt.pcap.Reader(f)
+f = open('../../pcaptest_20190516/0.pcap', 'rb')
+pcap = dpkt.pcap.Reader(f)
 
 
-# def inet_to_str(inet):
-#     try:
-#         return socket.inet_ntop(socket.AF_INET,inet)
-#     except:
-#         return socket.inet_ntop(socket.AF_INET6,inet)
+def inet_to_str(inet):
+    try:
+        return socket.inet_ntop(socket.AF_INET,inet)
+    except:
+        return socket.inet_ntop(socket.AF_INET6,inet)
 
-# port_list = []
-# num = 0
-# for ts,buf in pcap:
-#     num += 1
-#     print(num)
-#     ethData = dpkt.ethernet.Ethernet(buf) # 物理层
-#     ipData = ethData.data # 网络层
-#     transData = ipData.data # 传输层
-#     appData = transData.data # 应用层
+port_list = []
+num = 0
+for ts,buf in pcap:
+    num += 1
+    print(num)
+    ethData = dpkt.ethernet.Ethernet(buf) # 物理层
+    ipData = ethData.data # 网络层
+    transData = ipData.data # 传输层
+    appData = transData.data # 应用层
     
-#     ip_src = inet_to_str(ipData.src)
-#     ip_dst = inet_to_str(ipData.dst)
-#     print( "ip_src", ip_src)
-#     print("ip_dsr",ip_dst)
-#     print("src_port", transData.sport)
-#     print("dst_port", transData.dport)
-#     if transData.dport not in port_list:
-#         port_list.append(transData.dport)
+    ip_src = inet_to_str(ipData.src)
+    ip_dst = inet_to_str(ipData.dst)
+    print( "ip_src", ip_src)
+    print("ip_dsr",ip_dst)
+    print("src_port", transData.sport)
+    print("dst_port", transData.dport)
+    port_list.append(transData.dport) # 通过dst port分析
 
-# print("--------------------------------------")
-# print("pcap dst port type num:", len(port_list))
+print("--------------------------------------")
+print("pcap dst port type num:", len(port_list))
 
 port_official_csv = pd.read_csv("./service-names-port-numbers.csv")
-print(len(port_official_csv))
 
 # print(port_official_csv['Service Name'])
 # print(port_official_csv['Port Number'])
@@ -67,4 +65,23 @@ for i in range(0, len(port_official_csv)):
     if (port_official_csv['Service Name'][i] is not np.nan) and  (port_official_csv['Port Number'][i] is not np.nan):
         port_official_useful[port_official_csv['Port Number'][i]] = port_official_csv['Service Name'][i]
 
-print(len(port_official_useful))
+print("--------------------------------------")
+print("official useful port number:",len(port_official_useful))
+
+
+
+
+
+app_list = []
+for i in range(0, len(port_list)):
+	x = str(port_list[i])
+	if (x in port_official_useful.keys()):
+		app_list.append(port_official_useful[x]) 
+	else:
+		app_list.append('unknown')
+
+print("--------------------------------------")
+print("label set num:",len(app_list))
+
+for i in range(0, len(app_list)):
+	print(port_list[i], app_list[i])
