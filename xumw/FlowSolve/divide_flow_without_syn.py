b890cdc354d495a64705c99b8448d70aeae3b3a0
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2019-06-03 20:28:52 +0800

    finish feature csv

--- xumw/FlowSolve/divide_flow_without_syn.py
+++ xumw/FlowSolve/divide_flow_without_syn.py
@@ -26,8 +26,10 @@ validPacketNum = 0
 
 if __name__=="__main__":
     flows={}    #存放流对象的字典，key为元组 (源端口,目的地址,目的端口)，value为当前三元组下的所有包
+    flows_feature = []      # 存放流对象特征的列表，列表套列表，里面的每一个列表代表每一个流的特征
+    flows_label = []        # 存放流对象应用类型标签的列表
     index = 0
-    for i in range(0, 1):
+    for i in range(0, 30):
         print("start read " + str(i) + " pcap")
         pcap_path = '../../../pcaptest_20190516/' + str(i) + '.pcap'
         f = open(pcap_path, 'rb')
@@ -61,22 +63,45 @@ if __name__=="__main__":
                 flows[triple][index].append(len(buf)) # 特征 包长度
                 flows[triple][index].append(len(appData)) # 特征 有效负载长度
                 validPacketNum += 1
+        f.close()
         print(index)
         print("finish read " + str(i) + " pcap")
 
     # df = pd.DataFrame(flows)
     # df.to_csv('flows.csv')
-
-    TotalNum = 0
-    print("Total {} triples.".format(flows.__len__()))
-    print("Total {} valid packets.".format(validPacketNum))
+    flow_index = 0
     for key, flowGroup in flows.items():
-        TotalNum += 1
-        if TotalNum >= 10:
-            break
-        if len(flowGroup) != 1:
-            print("packet list", flowGroup)
+        print(key)
+        print(flowGroup)
+        feature = []#每个流的特征列表
+        
+        feature.append(len(flowGroup)) # 特征 包总数
+        
+        keys = list(flowGroup.keys())
+        duration = flowGroup[keys[len(keys)-1]][0] - flowGroup[keys[0]][0]
+        feature.append(duration) # 特征 连接的持续时间 若只有一个包 则为0
+        
+        packetLenMean = 0 # 特征 包的平均长度
+        payloadLenMean = 0 # 特征 有效负载平均长度
+        for index, item in flowGroup.items():
+            packetLenMean += item[2]
+            payloadLenMean += item[3]
+        packetLenMean = packetLenMean / len(keys)
+        payloadLenMean = payloadLenMean / len(keys)
+        feature.append(packetLenMean)
+        feature.append(payloadLenMean)
 
-    print(validPacketNum)
+        timeGapMean = 0 # 特征 包之间平均间隔 若只有一个包 则为0
+        if (len(keys) != 1):
+            for index in range(0, len(keys) - 1):
+                timeGapMean += flowGroup[keys[index + 1]][0] - flowGroup[keys[index]][0]
+            timeGapMean = timeGapMean / (len(keys) - 1)
+        feature.append(timeGapMean)
+
+        flows_feature.append(feature)
     
-    f.close()
+
+    print(flows_feature)
+    featureName=['PacketTotalNum','DurationTime','PacketLenMean', 'PayloadLenMean', 'TimeGapMean']
+    df=pd.DataFrame(columns=featureName,data=flows_feature)
+    df.to_csv('flows_feature.csv')
