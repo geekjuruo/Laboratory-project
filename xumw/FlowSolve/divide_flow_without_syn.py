d3f68f0346681c8a115f722998e9fa40502f219a
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2019-07-02 22:25:45 +0800

    rename some directory

--- xumw/FlowSolve/divide_flow_without_syn.py
+++ xumw/FlowSolve/divide_flow_without_syn.py
@@ -39,39 +39,40 @@ if __name__=="__main__":
     print("--------------------------------------")
     print("official useful port number:",len(port_official_useful))
 
-    for i in range(0, 1):
+    for i in range(0, 155):
         print("start read " + str(i) + " pcap")
-        pcap_path = '../../../pcaptest_20190516/' + str(i) + '.pcap'
+        pcap_path = '/data/liyinghui/' + str(i) + '.pcap'
         f = open(pcap_path, 'rb')
         pcap = dpkt.pcap.Reader(f)
         for ts, buf in pcap:
             index += 1
             ethData = dpkt.ethernet.Ethernet(buf)  # 物理层
             ipData = ethData.data  # 网络层
-            if not isinstance(ipData.data, dpkt.tcp.TCP): #流分析只需要TCP，UDP中不存在流的概念
-                continue
-            transData = ipData.data  # 传输层
-            appData = transData.data  # 应用层
+            if not isinstance(ipData, bytes):
+                if not isinstance(ipData.data, dpkt.tcp.TCP) and not isinstance(ipData.data, dpkt.udp.UDP): #流分析只需要TCP，UDP中不存在流的概念
+                    continue
+                transData = ipData.data  # 传输层
+                appData = transData.data  # 应用层
 
-            ip_dst = inet_to_str(ipData.dst)
-            port_src=transData.sport
-            port_dst=transData.dport
-            triple=(port_src, ip_dst, port_dst)
-            if triple not in flows:
-                flows[triple] = {}         # 流集合为一个字典结构，每一条流本身是一个字典结构
-                flows[triple][index] = []  # 每条流的每个包是一个列表结构，用以存放该包的特征
-                flows[triple][index].append(ts) # 特征 包时间戳
-                flows[triple][index].append(port_dst) # 特征 端口号
-                flows[triple][index].append(len(buf)) # 包长度
-                flows[triple][index].append(len(appData)) # 有效负载长度
-                validPacketNum += 1
-            else:
-                flows[triple][index] = []
-                flows[triple][index].append(ts) # 特征 包时间戳
-                flows[triple][index].append(port_dst) # 特征 端口号
-                flows[triple][index].append(len(buf)) # 特征 包长度
-                flows[triple][index].append(len(appData)) # 特征 有效负载长度
-                validPacketNum += 1
+                ip_dst = inet_to_str(ipData.dst)
+                port_src=transData.sport
+                port_dst=transData.dport
+                triple=(port_src, ip_dst, port_dst)
+                if triple not in flows:
+                    flows[triple] = {}         # 流集合为一个字典结构，每一条流本身是一个字典结构
+                    flows[triple][index] = []  # 每条流的每个包是一个列表结构，用以存放该包的特征
+                    flows[triple][index].append(ts) # 特征 包时间戳
+                    flows[triple][index].append(port_dst) # 特征 端口号
+                    flows[triple][index].append(len(buf)) # 包长度
+                    flows[triple][index].append(len(appData)) # 有效负载长度
+                    validPacketNum += 1
+                else:
+                    flows[triple][index] = []
+                    flows[triple][index].append(ts) # 特征 包时间戳
+                    flows[triple][index].append(port_dst) # 特征 端口号
+                    flows[triple][index].append(len(buf)) # 特征 包长度
+                    flows[triple][index].append(len(appData)) # 特征 有效负载长度
+                    validPacketNum += 1
         f.close()
         print(index)
         print("finish read " + str(i) + " pcap")
